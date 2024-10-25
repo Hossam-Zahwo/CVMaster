3f41493a12bcc61908e47502910fc68771a1fff2
hossam zahwo <158771871+Hossam-Zahwo@users.noreply.github.com>
2024-10-26 00:44:35 +0300

    Initial commit

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  languageOptions: {
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: __dirname,
-    },
-  },
-  // يمكن إضافة القواعد وغيرها من الإعدادات هنا
-};
